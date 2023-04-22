21175571bc4ca791b539842282ac1d716ad8c11e
Christian <christianday954@ymail.com>
2023-04-21 19:57:43 -0700

    Add css reset

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,14 +1,41 @@
+import React, {useState} from 'react'
 import loginCartoon from '../../assets/noodleGuy.jpg'
+import * as sessionActions from '../../store/session';
+import {useDispatch, useSelector} from 'react-redux'
+import './index.scss';
+import { Redirect } from 'react-router-dom';
+
 
 const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [errors, setErrors] = useState([]);
 
+    if (sessionUser) return <Redirect to='/' />; // change to grab cart items 
 
-
+    const handleSubmit = e => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ email, password }))
+            .catch(async (res) => {
+                let data;
+                try {
+                    data = await res.clone().json();
+                } catch {
+                    data = await res.text();
+                }
+                if (data?.errors) setErrors(data.errors);
+                else if (data) setErrors([data]);
+                else setErrors([res.statusText]);
+            });
+    }
 
     return ( 
         <>
             <div className="login-form-container">
-                <form className="login-form">
+                <form className="login-form" onSubmit={handleSubmit}>
                     <h1 id='login-header'>Log In</h1>
                     <div id='login-email'> 
                         <input 
