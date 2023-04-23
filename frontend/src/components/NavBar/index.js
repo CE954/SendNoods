d40c4205c95407e90b2402b515646b65b69564c6
Christian <christianday954@ymail.com>
2023-04-23 11:35:28 -0700

    Add responsive side-menu

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -6,14 +6,35 @@ import { FaSearch } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
 import { FaShoppingCart } from 'react-icons/fa';
 import { GoThreeBars } from 'react-icons/go';
+import { GrFormClose } from 'react-icons/gr';
 
 const NavBar = () => {
     const sessionUser = useSelector(state => state.session.user);
 
+    const toggleSideMenu = () => {
+        const sideNav = document.querySelector('.side-nav');
+        sideNav.classList.toggle('active');
+    }
+
     return (
         <div id='nav-bar'>
-            <div id='hamburger'> 
-                <GoThreeBars/>
+            <div className='hamburger'> 
+                <GoThreeBars id='burger-icon' onClick={toggleSideMenu}/>
+                <div className='side-nav'>
+                    <GrFormClose id='close-icon' onClick={toggleSideMenu}/>
+                    <NavLink to='/products' onClick={toggleSideMenu}>SHOP NOW</NavLink>
+                    <NavLink to='/about' onClick={toggleSideMenu}>OUR STORY</NavLink>
+                    {sessionUser ?
+                        <NavLink to='/account' onClick={toggleSideMenu}>ACCOUNT</NavLink> :
+                        <NavLink to='/login' onClick={toggleSideMenu}>ACCOUNT</NavLink>
+                    }
+                    <a href="https://www.linkedin.com/in/christian-espinosa-bb0b47187" target="_blank">
+                        LINKEDIN
+                    </a>
+                    <a href='https://github.com/CE954/SendNoods' target='_blank'>
+                        GITHUB
+                    </a>
+                </div>
             </div>
             <div id='nav-bar-left'>
                 <NavLink to='/' exact={true}>
