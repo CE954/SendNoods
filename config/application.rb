d780233339c7dbe829ef140cfdd2f3ba4783479e
Christian <christianday954@ymail.com>
2023-04-27 13:57:12 -0700

    Prep for deployment

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module SendNoods
     same_site: :lax,
     secure: Rails.env.production?
     # Configuration for the application, engines, and railties goes here.
+    config.railties_order = [:all, :main_app]
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
     resources :products, only: [:index, :show]
     resources :cart_items, only: [:create, :destroy, :update, :index]
   end
+
+  get '*path', to: "static_pages#frontend_index"
 end
