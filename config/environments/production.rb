0b181037a489143cc0263436cd61dfad79f1b0da
Christian <christianday954@ymail.com>
2023-04-28 16:24:57 -0700

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = "send_noods_production"
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = "send_noods_production"
 
   # config.action_mailer.perform_caching = false
 
